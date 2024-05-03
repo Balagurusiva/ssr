0082a0d8c6890e8fe6e550a37862d2ae53ddc644
Balaguru <75660556+Balagurusiva@users.noreply.github.com>
2024-05-03 14:20:26 +0530

    Add files via upload

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: false},
+  modules: ["@nuxt/ui", "@nuxt/image"],
+   render: {
+    ssr: false  
+  },
+  colorMode: {
+    preference: 'light'
+  }
+})
